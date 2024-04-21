4035c8140af3b06a4065fe04239ce24b4d736314
Fredy <142295949+fredrickomondiodongo@users.noreply.github.com>
2024-04-21 18:32:28 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,3 +10,25 @@ function playVideo(file){
     videoPlayer.style.display = "block";
     myVideo.muted = false;
 }
+
+
+// // Function to stop YouTube video playback
+// function stopYoutubeVideo() {
+//     // Add code to stop the YouTube video player
+//     document.getElementById("youtubePlayer").innerHTML = "";
+//     document.getElementById("youtubePlayer").style.display = "none";
+// }
+
+// // Function to play YouTube video
+// function playYoutubeVideo(videoId) {
+//     var embedCode = '<iframe  src="https://www.youtube.com/embed/' + videoId + '?autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+//     document.getElementById("youtubePlayer").innerHTML = embedCode;
+//     document.getElementById("youtubePlayer").style.display = "block";
+
+//     var youtubePlayerDiv =document.getElementById("youtubePlayer")
+//     youtubePlayerDiv.style.left = "50%";
+//     youtubePlayerDiv.style.top = "50%";
+//      youtubePlayerDiv.style.width = "750px";
+
+
+// }
